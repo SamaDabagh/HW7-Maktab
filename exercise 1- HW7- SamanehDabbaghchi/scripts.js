fdaf7435a61f73f170cafa525411ab9d5c889066
SAMDAB <s.dabbaghchi@gmail.com>
2022-05-11 16:35:13 +0430

    Circle Constructor

---
+++ exercise 1- HW7- SamanehDabbaghchi/scripts.js
@@ -0,0 +1,6 @@
+/*
+
+Ex.1
+Your task is to create a Circle constructor that creates a circle with a radius provided by an
+argument. The circles constructed must have two methods getArea() (PI*r^2) and
+getPerimeter() (2*PI*r) which give both respective areas and perimeter (circumference).*/
